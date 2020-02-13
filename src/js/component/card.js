cab7f7839c2c369a44af23f6fb27ac1797e949b8
Katoroc <aznkato@gmail.com>
2020-02-13 01:35:16 +0000

    to do list

---
+++ src/js/component/card.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+export function Card() {
+	const [list, setList] = useState([]);
+	const [content, setContent] = useState();
+	return (
+		<div className="card d-flex mx-auto" style={{ width: "32rem" }}>
+			<div className="card-body">
+				<ul className="list-group list-group-flush">
+					<li className="list-group-item">
+						<button
+							className="anything text-dark"
+							onClick={() => {
+								setList(list.concat(content));
+								setContent("");
+							}}>
+							Add to Do
+						</button>
+						<input
+							placeholder="Please add a task"
+							value={content}
+							onChange={e => setContent(e.target.value)}
+							onKeyPress={e => {
+								if (e.key === "Enter") {
+									setList(list.concat(content));
+									setContent("");
+								}
+							}}
+						/>
+						{list.map((item, index) => {
+							return <div key={index}>{item}</div>;
+						})}
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
+}
